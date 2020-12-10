99adbf4c41cc75f33a9ba3fcd1c68060afa458cd
zc <zhangchao19932008@163.com>
2020-12-10 17:38:36 +0800

    netty 入门: 实现netty版本的群发消息效果

--- src/main/java/netty/Client.java
+++ src/main/java/netty/Client.java
@@ -1,13 +1,14 @@
 package netty;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.EventExecutorGroup;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
@@ -27,48 +28,80 @@ public class Client {
         Bootstrap b = new Bootstrap();
 
         try {
-            ChannelFuture f=
-            b.group(group)
-                    .channel(NioSocketChannel.class)
+            ChannelFuture f =
+                    b.group(group)
+                            .channel(NioSocketChannel.class)
+                            //方法一:
 //                    .handler(new ChannelInitializer<SocketChannel>() {
 //                        @Override
 //                        protected void initChannel(SocketChannel ch) throws Exception {
 //                            //TODO
 //                        }
 //                    })
-                    //自定义handler
-                    .handler(new ClientChannelInitializer())
-                    .connect("localhost", 8888);
+                            //方法二:自定义handler
+                            .handler(new ClientChannelInitializer())
+                            .connect("localhost", 8888);
 //                    .addListener(null)
 //                    .sync();
             //连接监听器
-            f.addListener(new ChannelFutureListener(){
+            f.addListener(new ChannelFutureListener() {
                 @Override
                 public void operationComplete(ChannelFuture future) throws Exception {
-                    if(future.isSuccess()){
+                    if (future.isSuccess()) {
                         log.info("connected");
-                    }else {
+                    } else {
                         log.error("not connected");
                     }
                 }
             });
             f.sync();
-            log.info("。。。");
-        }
-        catch (InterruptedException e) {
+            f.channel().closeFuture().sync();//close()->ChannelFuture
+            log.info("client connect stopped");
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             group.shutdownGracefully();
         }
     }
 }
+
 @Slf4j
-class ClientChannelInitializer extends ChannelInitializer<SocketChannel>{
+class ClientChannelInitializer extends ChannelInitializer<SocketChannel> {
 
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
-        //TODO
-        log.info(ch+"来了来了");
+        ch.pipeline().addLast(new ChildHandler());
+    }
+
+    private class ChildHandler extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf buf = null;
+            int oldCount = 0;
+            try {
+                buf = (ByteBuf) msg;
+                oldCount=buf.refCnt();
+                log.info(String.valueOf(buf));
+                log.info(String.valueOf(buf.refCnt()));
+                byte[] bytes = new byte[buf.readableBytes()];
+                buf.getBytes(buf.readerIndex(), bytes);
+                log.info(new String(bytes));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (buf != null && oldCount==0) {
+                    ReferenceCountUtil.release(buf);
+                    log.info(String.valueOf(buf.refCnt()));
+                }
+            }
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            //channel 第一次连上可用 ，写出一个字符串Direct Memory
+            ByteBuf buf = Unpooled.copiedBuffer("Hello".getBytes());
+            ctx.writeAndFlush(buf);
+        }
     }
 }
--- src/main/java/netty/Server.java
+++ src/main/java/netty/Server.java
@@ -1,11 +1,20 @@
 package netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.GlobalEventExecutor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.nio.ByteBuffer;
+
 
 /**
  * @author czhang@mindpointeye.com
@@ -15,11 +24,78 @@ import java.net.Socket;
  */
 @Slf4j
 public class Server {
+    //默认的线程处理通道组上面的事件
+    public static ChannelGroup clients=new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
     public static void main(String[] args) throws IOException {
-        ServerSocket ss=new ServerSocket();
-        ss.bind(new InetSocketAddress(8888));
+        //只负责连接的线程池
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        //辅助处理业务的线程池
+        EventLoopGroup workGroup = new NioEventLoopGroup(2);
 
-        Socket s=ss.accept();
-        log.info("来客人了");
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            ChannelFuture f = b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            log.info(String.valueOf(Thread.currentThread().getId()));
+
+                            ChannelPipeline pl = ch.pipeline();
+                            pl.addLast(new ServerChildHandler());
+                        }
+                    })
+                    .bind(8888)
+                    .sync();
+            log.info("我的Netty开始服务");
+            //关闭程序
+            f.channel().closeFuture().sync();//close()->ChannelFuture
+            log.info("client connect stopped");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
+
+@Slf4j
+class ServerChildHandler extends ChannelInboundHandlerAdapter {//SimpleChannelInboundHandler Codec
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        log.info(String.valueOf(Thread.currentThread().getId()));
+        Server.clients.add(ctx.channel());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = null;
+        int oldCount=0;
+        try {
+            buf = (ByteBuf) msg;
+            log.info(String.valueOf(buf));
+            oldCount=buf.refCnt();
+            log.info(String.valueOf(buf.refCnt()));
+            byte[] bytes=new byte[buf.readableBytes()];
+            buf.getBytes(buf.readerIndex(),bytes);
+            log.info(new String(bytes));
+            //回写并关闭
+            Server.clients.writeAndFlush(buf);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (buf != null && oldCount==0) {
+                ReferenceCountUtil.release(buf);
+                log.info(String.valueOf(buf.refCnt()));
+            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
     }
 }
